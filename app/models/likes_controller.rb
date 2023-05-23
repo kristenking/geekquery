06e8b93d2388df4c5110127047cd760d1db2d7dd
kristenking <kristenking0110@gmail.com>
2023-05-22 17:21:15 -0700

    Added turbo streams to dyn update like status btn

--- app/models/likes_controller.rb
+++ app/models/likes_controller.rb
@@ -1,10 +1,21 @@
 class LikesController < ApplicationController
     before_action :set_question
+
     def toggle_like
         if(@like = @question.likes.find_by(user: current_user))
             @like.destroy
         else
             @question.likes.create(user: current_user)
+        end
+
+        respond_to do |format|
+            format.turbo_stream do
+              render turbo_stream: turbo_stream.replace(
+                "question#{@question.id}actions",
+                partial: "questions/question_actions",
+                locals: {question: @question}
+              )
+            end
         end
     end
 
