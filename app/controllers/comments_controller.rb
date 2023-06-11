8c2439b9af77b2fddc2b290ad9355b05dde28276
kristenking <kristenking0110@gmail.com>
2023-06-10 22:38:19 -0400

    added turbo stream for real-time rendering

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,32 @@
 class CommentsController < ApplicationController
-    before_action :set_question, only: [:create]
+  before_action :set_question, only: [:create]
+
   def create
     @comment = @question.comments.create(user: current_user, body: params[:comment_body])
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "question#{@question.id}comments",
+          partial: "questions/question_comments",
+          locals: { question: @question }
+        )
+      end
+      format.html { redirect_to @question }
+      format.json { render json: @comment, status: :ok }
+    end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
-    if(current_user == @comment.user)
+    if current_user == @comment.user
       @comment.destroy
     end
   end
 
-
   private
-    def set_question
-      @question = Question.find(params[:question_id])
-    end
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
 end
